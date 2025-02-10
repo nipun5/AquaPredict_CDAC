2a3c3ba7f88049dfb560a65b2864c28223add743
Nipun Jain <jainnipun891@gmail.com>
2025-02-10 12:43:41 +0530

    Initialize DVC

--- src/model/model_eval.py
+++ src/model/model_eval.py
@@ -3,6 +3,8 @@ import pandas as pd
 import pickle
 import json
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, roc_auc_score
+from dvclive import Live
+import yaml
 
 def load_data(data_path: str) -> pd.DataFrame:
     try:
@@ -29,12 +31,23 @@ def load_model(model_path: str) -> object:
 
 def evaluate_model(model: object, X: pd.DataFrame, y: pd.Series) -> dict:
     try:
+        params = yaml.safe_load(open("params.yaml","r"))
+        test_size = params["data_collection"]["test_size"]
+        n_estimators = params["data_modelbuilding"]["n_estimators"]
         y_pred = model.predict(X)
         acc = accuracy_score(y, y_pred)
         prec = precision_score(y, y_pred)
         recall = recall_score(y, y_pred)
         f1_s = f1_score(y, y_pred)
         roc_auc = roc_auc_score(y, y_pred)
+        with Live(save_dvc_exp = True) as live:
+            live.log("accuracy", acc)
+            live.log("precision", prec)
+            live.log("recall", recall)
+            live.log("f1_score", f1_s)
+            live.log("roc_auc", roc_auc)
+            live.log("test_size", test_size)
+            live.log("n_estimators", n_estimators)
         metrics_dict = {'accuracy': acc, 'precision': prec, 'recall': recall, 'f1_score': f1_s, 'roc_auc': roc_auc}
         return metrics_dict
     except Exception as e:
